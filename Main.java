dd3dc4640f9361547bcb1e8faf295969124fcf5d
MaxS2027 <max.schwartz2027@gbwl.org>
2026-01-23 13:56:27 +0000

    Committ

--- Main.java
+++ Main.java
@@ -2,5 +2,12 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+Trip MetsTrip = new Trip;
+public MetsTrip(String s, int ng, boolean t){
+   
+}
+
+
    }
+
 }
